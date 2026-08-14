15c388dba5c13d81ea2bf6786c1eff430072fd60
joroinnoroff <oinojorgen@gmail.com>
2026-08-15 00:54:51 +0200

    donee

--- components/WorksSection.tsx
+++ components/WorksSection.tsx
@@ -95,13 +95,13 @@ export default function WorksSection({ works }: WorksSectionProps) {
                 <div className="flex w-fit flex-col">
                   <Link
                     href={`/shop/${p.id}`}
-                    className="relative mb-4 block h-[min(62vh,440px)] w-fit overflow-hidden bg-[#eee] md:h-[min(62vh,520px)]"
+                    className="relative mb-4 block w-fit overflow-hidden bg-[#eee]"
                   >
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                       src={p.image}
                       alt={p.title}
-                      className="block h-full w-auto max-w-[min(86vw,820px)] select-none md:max-w-[min(75vw,900px)]"
+                      className="block h-auto max-h-[min(62vh,440px)] w-auto max-w-[min(86vw,820px)] select-none md:max-h-[min(62vh,520px)] md:max-w-[min(75vw,900px)]"
                       draggable={false}
                     />
                   </Link>
